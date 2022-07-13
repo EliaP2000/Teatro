b19e7cea217cba20408099d4a4067666c04ee1c3
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-13 11:20:48 +0200

    3 files modified, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,38 @@ var BloccoTeatro = Array.from(
 export class AppComponent {
   public value: any;
   public style: CSSStyleDeclaration;
-  
+  bottoni = [];
+  bottoni1 = [];
+
+  constructor() {
+    this.bottoni = Array(70)
+      .fill(0)
+      .map((x, i) => i + 1);
+    this.bottoni1 = Array(24)
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+
+  public selezionaPostoPlatea(info: string, Numplatea: any) {
+    for (var j = 0; j < this.bottoni.length; j++) {
+      if (Numplatea == j) {
+        this.bottoni[j] = info;
+        //this.bottoni[j].style.backgroundColor = "red";
+        info = '';
+      }
+    }
+  }
+
+  public selezionaPostoPalchi(info: string, Numpalchi: any) {
+    for (var j = 0; j < this.bottoni1.length; j++) {
+      if (Numpalchi == j) {
+        this.bottoni1[j] = info;
+        //this.bottoni1[j].style.backgroundColor = "red";
+        info = '';
+      }
+    }
+  }
+}
   EntryDatabase(key: string) {
     var w = Array.from(
       document.getElementsByClassName('divEntry') as unknown as HTMLCollectionOf<HTMLElement>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TeatroComponent } from '../teatro/teatro.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TeatroComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 
