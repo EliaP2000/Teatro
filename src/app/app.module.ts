f393b55d44f6cae57106c94c70fa808c69378ac8
EliaP2000 <100145261+EliaP2000@users.noreply.github.com>
2022-09-19 12:02:18 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { add_nome_component } from './add_nome/add_nome.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent, add_nome_component]
 })
 
 export class AppModule { }
