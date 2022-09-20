2abb3f75c1cf857bf6c1caa7c98639321dd80ba6
EliaP2000 <100145261+EliaP2000@users.noreply.github.com>
2022-09-20 11:52:03 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { add_nome_component } from './add_nome/add_nome.component';
-import { clearTeatro_component } from './clearTeatro/clearTeatro.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent, add_nome_component, clearTeatro_component]
+  bootstrap:    [ AppComponent, add_nome_component ]
 })
 
 export class AppModule { }
