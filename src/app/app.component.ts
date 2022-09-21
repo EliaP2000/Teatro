daf51d22fe246aebcdb80080437a8db6c69953ed
EliaP2000 <100145261+EliaP2000@users.noreply.github.com>
2022-09-21 15:59:36 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,19 +25,19 @@ export class AppComponent {
       .map((x, i) => i + 1);
   }
 
-  public selezionaPostoPlatea(info: string, counter1: any, counter2: any) {
+  public selezionaPostoPlatea(counter1: any, counter2: any) {
     this.righe = counter1.toString();
     this.colonne = counter2.toString();
     var sum = this.righe+this.colonne;
     for (var j = 0; j < this.bottoni.length; j++) {
       if (sum==j) {
-        if((info!='')&&(info!=this.bottoni[j])&&(typeof this.bottoni[j]!=='string')){
-            document.getElementById('outputPrenotazione').innerHTML = 'Posto in Platea ' + (j+1) + ' prenotato per ' + info;
-            this.bottoni[j] = info;
+        if((this.nominativo!='')&&(this.nominativo!=this.bottoni[j])&&(typeof this.bottoni[j]!=='string')){
+            document.getElementById('outputPrenotazione').innerHTML = 'Posto in Platea ' + (j+1) + ' prenotato per ' + this.nominativo;
+            this.bottoni[j] = this.nominativo;
             this.home=undefined;
-        } else if(info==this.bottoni[j]){
+        } else if(this.nominativo==this.bottoni[j]){
             document.getElementById('outputPrenotazione').innerHTML = 'Hai già prenotato il posto ' + (j+1);
-        } else if((info!='')&&(typeof this.bottoni[j]==='string')){
+        } else if((this.nominativo!='')&&(typeof this.bottoni[j]==='string')){
             document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + (j+1) +' già prenotato da: ' + this.bottoni[j];
         }
         else {
@@ -47,7 +47,7 @@ export class AppComponent {
     }
   }
 
-  public selezionaPostoPalchi(info: string, counter1: any, counter2: any,) {
+  public selezionaPostoPalchi(counter1: any, counter2: any,) {
     this.righe = counter1.toString();
     this.colonne = counter2.toString();
     var sum = this.righe+this.colonne;
@@ -59,13 +59,13 @@ export class AppComponent {
       sum = sum-12;
     for (var j = 0; j < 36; j++) {
       if (sum==j) {
-        if((info!='')&&(info!=this.bottoni1[j])&&(typeof this.bottoni1[j]!=='string')){
-            document.getElementById('outputPrenotazione').innerHTML = 'Posto sul Palco ' + (j+1) + ' prenotato per ' + info;
-          this.bottoni1[j] = info;
+        if((this.nominativo!='')&&(this.nominativo!=this.bottoni1[j])&&(typeof this.bottoni1[j]!=='string')){
+            document.getElementById('outputPrenotazione').innerHTML = 'Posto sul Palco ' + (j+1) + ' prenotato per ' + this.nominativo;
+          this.bottoni1[j] = this.nominativo;
           this.home=undefined;
-        } else if(info==this.bottoni1[j]){
+        } else if(this.nominativo==this.bottoni1[j]){
             document.getElementById('outputPrenotazione').innerHTML = 'Hai già prenotato il posto ' + (j+1);
-        } else if((info!='')&&(typeof this.bottoni1[j]==='string')){
+        } else if((this.nominativo!='')&&(typeof this.bottoni1[j]==='string')){
             document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + (j+1) +' già prenotato da: ' + this.bottoni1[j];
         }
         else {
