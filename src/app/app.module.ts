43e918e63feff9e460f3ff46875671e688bb82e1
EliaP2000 <100145261+EliaP2000@users.noreply.github.com>
2022-09-22 16:27:46 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { add_nome_component } from './add_nome/add_nome.component';
 import { accessTeatro_component } from './accessTeatro/accessTeatro.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, add_nome_component, accessTeatro_component ],
   bootstrap:    [ AppComponent ]
 })
