50a7e48aec12559dc8faea91e04c3c8e7e4d2a9a
EliaP2000 <100145261+EliaP2000@users.noreply.github.com>
2022-09-29 11:41:31 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   home: string = "";
   nominativo = "";
   chiave_teatro = "";
+  result;
   constructor(private service: teatro_service) {
     this.bottoni = Array(70)
       .fill(1)
@@ -35,38 +36,15 @@ export class AppComponent {
     var PostoSelezionato_Array = this.righe+this.colonne;
     var PostoScelto_HTML = counter1+((counter2+1).toString());
     const teatroIntero = this.bottoni.concat(this.bottoni1);
-    if((this.nominativo!='')&&(this.bottoni[PostoSelezionato_Array]==undefined)){
-      this.bottoni[PostoSelezionato_Array] = this.nominativo;
-      this.chiave_teatro = this.div;
-        this.service.set_Postiteatro(this.chiave_teatro, teatroIntero).subscribe({
-          next: () => {
-            document.getElementById('outputPrenotazione').innerHTML = 'Posto in Platea ' + PostoScelto_HTML + ' prenotato per ' + this.nominativo;
-          },
-          error: (err) => console.error(`Errore nell'observer: ${JSON.stringify(err)}`),
-        });
-        this.home=undefined;
-        console.log(this.bottoni)
-    } 
-    
-    else if((this.nominativo!='')&&(this.bottoni[PostoSelezionato_Array]!=undefined)&&(this.bottoni[PostoSelezionato_Array]==this.nominativo)){
-      document.getElementById('outputPrenotazione').innerHTML = 'Hai già prenotato questo posto';
-    }
-    
-    else if((this.nominativo!='')&&(this.bottoni[PostoSelezionato_Array]!=undefined)&&(this.bottoni[PostoSelezionato_Array]!=this.nominativo)){
-      document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + ' già prenotato da: ' + this.bottoni[PostoSelezionato_Array];
-    }
-    
-    else if((this.nominativo=='')&&(this.bottoni[PostoSelezionato_Array]!=undefined)){
-      document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + ' già prenotato da: ' + this.bottoni[PostoSelezionato_Array];
-    }
-    
-    else {
-      document.getElementById('outputPrenotazione').innerHTML = 'Posto libero - Inserisci il nominativo';
-    }
+    return this.result = this.selezionaPosto(PostoSelezionato_Array, PostoScelto_HTML, teatroIntero, this.bottoni);
   }
 
   public selezionaPostoPalchi(counter1: any, counter2: any) {
-    this.righe = counter1.toString();
+    if(counter1>=1){
+      this.righe = '0' + ((counter1).toString());
+    }else{
+      this.righe = counter1.toString();
+    }
     this.colonne = counter2.toString();
     var sum = this.righe+this.colonne;
     if(sum>=10&&sum<=15)
@@ -75,21 +53,38 @@ export class AppComponent {
       sum = sum-8;
     else if(sum>=30&&sum<=35)
       sum = sum-12;
-    for (var j = 0; j < 36; j++) {
-      if (sum==j) {
-        if((this.nominativo!='')&&(this.nominativo!=this.bottoni1[j])&&(typeof this.bottoni1[j]!=='string')){
-            document.getElementById('outputPrenotazione').innerHTML = 'Posto sul Palco ' + (j+1) + ' prenotato per ' + this.nominativo;
-          this.bottoni1[j] = this.nominativo;
-          this.home=undefined;
-        } else if(this.nominativo==this.bottoni1[j]){
-            document.getElementById('outputPrenotazione').innerHTML = 'Hai già prenotato il posto ' + (j+1);
-        } else if((this.nominativo!='')&&(typeof this.bottoni1[j]==='string')){
-            document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + (j+1) +' già prenotato da: ' + this.bottoni1[j];
-        }
-        else {
-          document.getElementById('outputPrenotazione').innerHTML = 'Casella Nome vuoto';
-        }
-      }
+    var sum_HTML = counter1+((counter2+1).toString());
+    const teatroIntero = this.bottoni.concat(this.bottoni1);
+    return this.result = this.selezionaPosto(sum, sum_HTML, teatroIntero, this.bottoni1);
+  }
+
+  selezionaPosto(PostoInArray: string|number, PostoInHTML: string, teatroIntero: any[], array: any[]){
+    if((this.nominativo!='')&&(array[PostoInArray]==undefined)){
+      array[PostoInArray] = this.nominativo;
+      this.chiave_teatro = this.div;
+        this.service.set_Postiteatro(this.chiave_teatro, teatroIntero).subscribe({
+          next: () => {
+            document.getElementById('outputPrenotazione').innerHTML = 'Posto sul Palco ' + PostoInHTML + ' prenotato per ' + this.nominativo;
+          },
+          error: (err) => console.error(`Errore nell'observer: ${JSON.stringify(err)}`),
+        });
+        this.home=undefined;
+        console.log(array)
+    } 
+    else if((this.nominativo!='')&&(array[PostoInArray]!=undefined)&&(array[PostoInArray]==this.nominativo)){
+      document.getElementById('outputPrenotazione').innerHTML = 'Hai già prenotato questo posto';
+    }
+    
+    else if((this.nominativo!='')&&(array[PostoInArray]!=undefined)&&(array[PostoInArray]!=this.nominativo)){
+      document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + ' già prenotato da: ' + array[PostoInArray];
+    }
+    
+    else if((this.nominativo=='')&&(array[PostoInArray]!=undefined)){
+      document.getElementById('outputPrenotazione').innerHTML = 'Posto ' + ' già prenotato da: ' + array[PostoInArray];
+    }
+    
+    else {
+      document.getElementById('outputPrenotazione').innerHTML = 'Posto libero - Inserisci il nominativo';
     }
   }
 
